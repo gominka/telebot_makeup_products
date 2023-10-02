737939a92082e5116b4f85d5627c5c3dcc823280
daria <daria.maksimova2020@gmail.com>
2023-10-02 15:37:20 +0300

    Создание основного функционала

--- handlers/default_handlers/commands.py
+++ handlers/default_handlers/commands.py
@@ -1,9 +1,16 @@
-from telebot.types import Message
+from telebot import types
 
-from config_data.config import CUSTOM_COMMANDS
 from loader import bot
 
+from config_data.config import CUSTOM_COMMANDS
+from keyboards.reply.start_btn import btn_start
+
 text_messages = {
+    'start':
+        u'Приветствуем Вас, '
+        u'{message.from_user.first_name}, '
+        u'в нашем телеграм боте \n\n'
+        u'Выберите условие для поиска',
     'info':
         u'Список доступных команд: \n'
         u'{commands} \n'
@@ -11,11 +18,15 @@ text_messages = {
 
 
 @bot.message_handler(commands=['info'])
-def bot_info(message):
+def bot_info(message: types.Message):
     print('1')
     text = [f"/{command} - {desk}" for command, desk in CUSTOM_COMMANDS]
     bot.reply_to(message, text_messages['info'].format(commands="\n".join(text)))
 
 
-
-
+@bot.message_handler(commands=['start'])
+def bot_start(message: types.Message):
+    bot.send_message(message.chat.id, f'Приветствуем Вас, '
+                                      f'{message.from_user.first_name}, '
+                                      f'в нашем телеграм боте \n\n'
+                                      f'Выберите условие для поиска', reply_markup=btn_start)
