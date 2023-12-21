c8d46d26a5b2be52f80f45e07f83d58f4a21aa49
daria <daria.maksimova2020@gmail.com>
2023-12-21 18:31:41 +0300

    Refactoring code

--- loader.py
+++ loader.py
@@ -3,3 +3,4 @@ from config_data import config
 
 storage = StateMemoryStorage()
 bot = TeleBot(token=config.BOT_TOKEN, state_storage=storage)
+
--- main.py
+++ main.py
@@ -10,3 +10,4 @@ if __name__ == '__main__':
     bot.add_custom_filter(StateFilter(bot))
     set_default_commands(bot)
     bot.infinity_polling(skip_pending=True)
+
